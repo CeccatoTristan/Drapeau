63d2667ab7f72481db544516b112dd66e7fdd074
Tristan Ceccato <ceccatot@wificampus-068173.grenet.fr>
2018-12-03 08:13:14 +0100

    Initial Commit

---
+++ Drapeau/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Drapeau
+//
+//  Created by Tristan Ceccato on 03/12/2018.
+//  Copyright © 2018 Tristan Ceccato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DrapeauTests/DrapeauTests.swift
@@ -0,0 +1,34 @@
+//
+//  DrapeauTests.swift
+//  DrapeauTests
+//
+//  Created by Tristan Ceccato on 03/12/2018.
+//  Copyright © 2018 Tristan Ceccato. All rights reserved.
+//
+
+import XCTest
+@testable import Drapeau
+
+class DrapeauTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

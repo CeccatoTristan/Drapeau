3ef7420255a69ec2513577a53cfbd28e4774f40c
Unknown <ceccato.tristan@gmail.com>
2018-12-07 12:19:36 +0100

    Correctif et menu complet

---
+++ Drapeau/Controller/SurvieHardViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SurvieHardViewController.swift
+//  Drapeau
+//
+//  Created by Tristan Ceccato on 07/12/2018.
+//  Copyright © 2018 Tristan Ceccato. All rights reserved.
+//
+
+import UIKit
+
+class SurvieHardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

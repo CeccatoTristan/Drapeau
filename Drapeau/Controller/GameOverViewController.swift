327705ee2cca539e96633c174cbdbdd3d75d9c5b
Unknown <ceccato.tristan@gmail.com>
2018-12-10 13:28:51 +0100

    On avance

---
+++ Drapeau/Controller/GameOverViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameOverViewController.swift
+//  Drapeau
+//
+//  Created by Tristan Ceccato on 10/12/2018.
+//  Copyright © 2018 Tristan Ceccato. All rights reserved.
+//
+
+import UIKit
+
+class GameOverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
